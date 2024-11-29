0f52b1a95bf4fbbb9bc3779b8f59bc6f78c24802
moyos0reoluwa <oreoluwaoyedele2@gmail.com>
2024-11-29 22:38:34 +0100

    Update next.config.mjs for configuration changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true, basePath: 'client', assetPrefix: 'client',
+};
 
 export default nextConfig;
