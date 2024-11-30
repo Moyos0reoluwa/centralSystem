abf3c7f2ac3f810d474c29747b54668253d79db0
moyos0reoluwa <oreoluwaoyedele2@gmail.com>
2024-11-30 02:10:00 +0100

    Update GitHub Actions workflow for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, basePath: '/client', assetPrefix: '/client/',
+    reactStrictMode: true, basePath: '/client', assetPrefix: '/client/', output: 'export'
 };
 
 export default nextConfig;
