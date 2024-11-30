a91a1c68d3df1372a5dee6a784cc0e4d09d0e1a9
moyos0reoluwa <oreoluwaoyedele2@gmail.com>
2024-11-30 01:37:27 +0100

    Update basePath in next.config.mjs to start with '/'

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, basePath: 'client', assetPrefix: 'client',
+    reactStrictMode: true, basePath: '/client', assetPrefix: '/client/',
 };
 
 export default nextConfig;
